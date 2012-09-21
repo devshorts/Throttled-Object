7b8ad74cd0c0d8262016ab69d00d360094c0fc8d
anton <akropp@gmail.com>
2012-09-20 20:31:52 -0400

    adding unit tests

---
+++ ThrottledObjectTests/ThrottledObjectTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+using NUnit.Framework;
+using ThrottedObject;
+
+namespace ThrottledObjectTests
+{
+    [TestFixture]
+    public class ThrottledObjectTest
+    {
+        [Test]
+        public void TestThrottle()
+        {
+            var throttle = new ThrottledObject<TestItem>(TimeSpan.FromMilliseconds(2000), Generator);
+
+            Observable.Interval(TimeSpan.FromMilliseconds(500))
+                .Subscribe(_ =>
+                               Console.WriteLine("[now: {0}] - Last time generated: {1}",
+                                                         DateTime.Now,
+                                                         throttle.LazyGetItem.TimeGenerated));
+
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+        }
+
+        public TestItem Generator()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            return new TestItem
+                       {
+                           TimeGenerated = DateTime.Now
+                       };
+        }
+    }
+
+    public class TestItem
+    {
+        public DateTime TimeGenerated { get; set; }
+    }
+}
